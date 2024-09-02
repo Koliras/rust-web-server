79b764d7bcda5e7b6e91e4c13810343f38086f31
Koliras <gyrmanchyk230506@gmail.com>
2024-09-02 17:43:32 +0000

    make server multithreaded

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+use rust_web_server::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufRead, BufReader},
     net::{self, TcpStream},
+    thread::sleep,
+    time::Duration,
 };
 
 fn main() {
     let listener =
         net::TcpListener::bind("127.0.0.1:3000").expect("Could not listen to provided address");
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
-        match stream {
+        pool.execute(|| match stream {
             Ok(s) => handle_connection(s),
             Err(e) => println!("invalid stream, {:?}", e),
-        }
+        });
     }
 }
 
@@ -20,10 +25,13 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT_FOUND", "404.html")
+    let (status_line, file_name) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT_FOUND", "404.html"),
     };
     let contents = fs::read_to_string(file_name).unwrap();
     let length = contents.len();
